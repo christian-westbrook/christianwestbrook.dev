800068f8bf6e8e1e1ff4d5f95edeff6935feab29
christian-westbrook <christian.a.westbrook@proton.me>
2025-09-05 15:01:59 -0500

    feat: Added an initial BlogPost component with a loading animation

---
+++ src/frontend/src/components/BlogPost/BlogPost.tsx
@@ -0,0 +1,13 @@
+// Libraries
+import type { JSX } from 'react'
+
+// Styles
+import './BlogPost.scss'
+
+function BlogPost(): JSX.Element {
+    return (
+        <span className="loading-animation"></span>
+    )
+}
+
+export default BlogPost;
