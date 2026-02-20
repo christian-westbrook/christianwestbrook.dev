3e56df03a1369c196cc0150c9a0cc2761a55f272
christian-westbrook <christian.a.westbrook@proton.me>
2026-02-20 14:24:58 -0600

    feat: Added a brief introduction to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ function App() {
   return (
     <>
       <h1>Hi, I'm Christian!</h1>
+      <p>I'm a Software Engineer based out of Northwest Arkansas.</p>
+      <p>This website is my playground (currently under construction). I sincerely hope you enjoy!</p>
     </>
   )
 }
